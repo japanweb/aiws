1d7c674f14d7de28a68bec6e68ec1045cf45c675
japanweb <syoueiwa@gmail.com>
2021-12-08 17:49:21 +0900

    解决 WSL2 linux权限问题 chmod(): Operation not permitted

--- inst-wsl-bota.ps1
+++ inst-wsl-bota.ps1
@@ -202,7 +202,7 @@ function clearFile() {
     wsl sh -c "yum install openssh-server -y | sshd-keygen"
     wsl sh -c "mv /usr/bin/systemctl /usr/bin/systemctl.old -f"
     wsl sh -c "curl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py > /usr/bin/systemctl"
-    wsl sh -c "echo '[network]' > /etc/wsl.conf | echo 'generateResolvConf=false' >> /etc/wsl.conf"
+    wsl sh -c "echo '[network]' > /etc/wsl.conf | echo 'generateResolvConf=false' >> /etc/wsl.conf | echo '[automount]' >> /etc/wsl.conf | echo 'enabled = true' >> /etc/wsl.conf | echo 'root = /mnt/' >> /etc/wsl.conf | echo 'options = \""metadata,umask=22,fmask=111\""' >> /etc/wsl.conf | echo 'mountFsTab = true' >> /etc/wsl.conf | echo '[filesystem]' >> /etc/wsl.conf | echo 'umask = 022' >> /etc/wsl.conf"
     # wsl sh -c "mv /etc/resolv.conf /etc/resolv.conf.wsl"
     wsl sh -c "echo '[nameserver]' > /etc/resolv.conf | echo 'nameserver 8.8.8.8' >> /etc/resolv.conf | echo 'nameserver 8.8.4.4' >> /etc/resolv.conf"
     wsl sh -c "sudo chmod +x /usr/bin/systemctl"
